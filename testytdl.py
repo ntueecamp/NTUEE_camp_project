0bb1301781968d76c05c98306ad49fddc1f0c570
HenningY <79901008+HenningY@users.noreply.github.com>
2021-08-04 00:24:35 +0800

    Update testytdl.py

--- testytdl.py
+++ testytdl.py
@@ -1,9 +1,24 @@
 import youtube_dl
+import os
 
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+link = 'https://www.youtube.com/watch?v=Z1eiALxC_9I'
 
 with ydl:
-    result = ydl.extract_info(
-        'https://www.youtube.com/watch?v=Z1eiALxC_9I',
-        download=True
-    )
+    result = ydl.extract_info(link, download=True)
+    video_info = result.get('title')
+
+print(video_info)
+
+Video_name = link.replace('https://www.youtube.com/watch?v=', '')
+Video_name += '.mp4'
+
+filename = os.path.abspath(Video_name)
+Dirname = os.path.dirname(Video_name)
+
+new_name = Dirname + video_info + '.mp4'
+
+if os.path.exists(filename):
+    print("完整路徑名稱: "+filename)
+
+os.rename(filename, new_name)
